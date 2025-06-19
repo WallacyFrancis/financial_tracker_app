953eb50aee90a3b6441aa9ec5f8cb4a91397a64d
WallacyFrancis <wallacy_francis@hotmail.com>
2025-06-19 19:39:49 -0300

    diciona classes Asset e TimeSeriesPoint com construtores e métodos para manipulação de dados.

--- lib/models/asset_model.dart
+++ lib/models/asset_model.dart
@@ -1,1 +1,26 @@
- 
+class Asset {
+  final String symbol;
+  final String name;
+  final String description;
+  final String currency;
+  final String country;
+
+  Asset({
+    required this.symbol,
+    required this.name,
+    required this.description,
+    required this.currency,
+    required this.country,
+  });
+
+  // Factory constructor para criar um Asset a partir do JSON da API
+  factory Asset.fromJson(Map<String, dynamic> json) {
+    return Asset(
+      symbol: json['Symbol'] ?? 'N/A',
+      name: json['Name'] ?? 'N/A',
+      description: json['Description'] ?? 'No description available.',
+      currency: json['Currency'] ?? 'N/A',
+      country: json['Country'] ?? 'N/A',
+    );
+  }
+}
--- lib/models/time_series_model.dart
+++ lib/models/time_series_model.dart
@@ -1,1 +1,6 @@
- 
+class TimeSeriesPoint {
+  final DateTime time;
+  final double close;
+
+  TimeSeriesPoint({required this.time, required this.close});
+}
